38d6df8db206c064c7a85530a54f983cac156269
wangyong <wangyongyeah@126.com>
2016-06-01 21:35:50 +0800

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularBootstrapDemoApp
+ * @description
+ * # angularBootstrapDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularBootstrapDemoApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBootstrapDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBootstrapDemoApp
+ */
+angular.module('angularBootstrapDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
