08889f4dd21d3cb63c9dcc323df2ed33e042dfe7
wangyong <wy33082@tcent.cn>
2016-06-13 16:55:21 +0800

    添加controller文件

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,77 +1,36 @@
-/**
- * Created by wy33082 on 2016/6/6.
- */
-/*
- var sixRealmsApp = angular.module('sixRealmsApp', ['ngRoute']);
- function menu_route($routeProvider) {
- $routeProvider.
- when('blank-page', {templateUrl:'views/main/blank-page.html',controller:'blankController'});
- };
- sixRealmsApp.config(menu_route);
-
- sixRealmsApp.controller('blankController',function($scope){
-
- });
- sixRealmsApp.controller('menuController',function($scope){
-
- });*/
-/*
- var sixRealmsApp = angular.module('sixRealmsApp', ['ngRoute'])
- .controller('menuController', function($scope) {
- })
- .config(function($routeProvider, $locationProvider) {
- $routeProvider
- .when('/blank-page', {
- templateUrl: 'views/main/blank-page.html',
- controller: 'blankController'
- });
- // configure html5
- //$locationProvider.html5Mode(true);
- });
-
- sixRealmsApp.controller('blankController',function($scope){
-
- });*/
-
-/*
-$(function(){
-    $("#logout").click(function(){
-        $("#logoutModal").modal("show");
-    });
-});
-*/
+'use strict';
 
 angular.module('sixRealmsApp', ['ui.router'])
-    .controller('menuController',function($scope){
+    .controller('menuController', function ($scope) {
 
     })
-    .config(function($stateProvider) {
+    .config(function ($stateProvider) {
         $stateProvider
             .state('config-manage', {
                 url: '/config-manage',
                 templateUrl: 'views/main/config-manage.html',
-                controller: function($scope){
+                controller: function ($scope) {
 
                 }
             })
             .state('black-white-list', {
-            url: '/black-white-list',
-            templateUrl: 'views/main/black-white-list.html',
-            controller: function($scope){
+                url: '/black-white-list',
+                templateUrl: 'views/main/black-white-list.html',
+                controller: function ($scope) {
 
                 }
             })
             .state('engine-rules', {
                 url: '/engine-rules',
                 templateUrl: 'views/main/engine-rules.html',
-                controller: function($scope){
+                controller: function ($scope) {
 
                 }
             })
             .state('user-roles', {
                 url: '/user-roles',
                 templateUrl: 'views/main/user-roles.html',
-                controller: function($scope){
+                controller: function ($scope) {
 
                 }
             })
